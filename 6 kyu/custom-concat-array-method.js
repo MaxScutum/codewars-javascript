5c612a77352d3588c8b643bb20a9b694acf31a9d
MaxScutum <doncarlos1605@gmail.com>
2026-08-23 23:19:33 +0700

    update custom-concat-array-method.js

--- 6 kyu/custom-concat-array-method.js
+++ 6 kyu/custom-concat-array-method.js
@@ -19,3 +19,8 @@ Array.prototype.customConcat = function (...args) {
 
   return result;
 };
+
+// test cases
+const numbers = [1, 2, 3];
+const result = numbers.customConcat(4, 5, 6);
+console.log(result); // [1, 2, 3, 4, 5, 6]
