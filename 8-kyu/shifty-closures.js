14f61c48c3904d9ad1a4501feeed073e471d8eeb
MaxScutum <doncarlos1605@gmail.com>
2026-09-12 19:56:10 +0700

    add shifty-closures.js

---
+++ 8-kyu/shifty-closures.js
@@ -0,0 +1,12 @@
+function greetAbe() {
+  const name = "Abe";
+  return "Hello, " + name + "!";
+}
+
+function greetBen() {
+  const name = "Ben";
+  return "Hello, " + name + "!";
+}
+
+console.log(greetAbe());
+console.log(greetBen());
